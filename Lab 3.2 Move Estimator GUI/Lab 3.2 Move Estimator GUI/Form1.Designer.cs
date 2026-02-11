673b833d4d5cc890cbb0030f5b2b87d325a698aa
Xavier Soule <xavierjsoule@gmail.com>
2026-02-10 21:44:45 -0500

    Add files via upload

---
+++ Lab 3.2 Move Estimator GUI/Lab 3.2 Move Estimator GUI/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Lab_3._2_Move_Estimator_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.hourBox = new System.Windows.Forms.TextBox();
+            this.mileBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 175);
+            this.label1.TabIndex = 101;
+            this.label1.Text = "Rates : \r\n\r\nBase Rate = $200\r\n\r\nHourly Pay = $150\r\n\r\nMileage = $2 per mile\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(276, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 25);
+            this.label2.TabIndex = 100;
+            this.label2.Text = "Hourly Pay :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(315, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.TabIndex = 99;
+            this.label3.Text = "Miles :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(230, 356);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 25);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hourBox
+            // 
+            this.hourBox.Location = new System.Drawing.Point(411, 132);
+            this.hourBox.Name = "hourBox";
+            this.hourBox.Size = new System.Drawing.Size(100, 20);
+            this.hourBox.TabIndex = 0;
+            // 
+            // mileBox
+            // 
+            this.mileBox.Location = new System.Drawing.Point(396, 177);
+            this.mileBox.Name = "mileBox";
+            this.mileBox.Size = new System.Drawing.Size(100, 20);
+            this.mileBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mileBox);
+            this.Controls.Add(this.hourBox);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Move Estimator ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox hourBox;
+        private System.Windows.Forms.TextBox mileBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab 3.2 Move Estimator GUI/Lab 3.2 Move Estimator GUI/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3._2_Move_Estimator_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double miles = Convert.ToDouble(mileBox.Text) * 2,
+                   hours = Convert.ToDouble(hourBox.Text) * 150;
+
+            double cost = miles + hours + 200;
+            string costFormat = cost.ToString("C2");
+
+            outputLabel.Text = $"Move will cost : {costFormat}";
+        }
+    }
+}
