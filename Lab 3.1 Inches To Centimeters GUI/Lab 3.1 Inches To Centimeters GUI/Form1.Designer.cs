452e97261ac27e3b860cb8302f8bc5aec63e1cd8
Xavier Soule <xavierjsoule@gmail.com>
2026-02-10 21:43:24 -0500

    Add files via upload

---
+++ Lab 3.1 Inches To Centimeters GUI/Lab 3.1 Inches To Centimeters GUI/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Lab_3._1_Inches_To_Centimeters_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inchesBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(64, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 25);
+            this.label1.TabIndex = 100;
+            this.label1.Text = "This Program will convert inches to centimeters!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(179, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 99;
+            this.label2.Text = "Inches :";
+            // 
+            // inchesBox
+            // 
+            this.inchesBox.Location = new System.Drawing.Point(272, 95);
+            this.inchesBox.Name = "inchesBox";
+            this.inchesBox.Size = new System.Drawing.Size(100, 20);
+            this.inchesBox.TabIndex = 0;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.calcButton.Location = new System.Drawing.Point(227, 134);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(103, 43);
+            this.calcButton.TabIndex = 1;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.outputLabel.Location = new System.Drawing.Point(179, 207);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 25);
+            this.outputLabel.TabIndex = 101;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(593, 313);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.inchesBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Inches To Centimeters";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inchesBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Lab 3.1 Inches To Centimeters GUI/Lab 3.1 Inches To Centimeters GUI/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3._1_Inches_To_Centimeters_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inchesBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            double inches = Convert.ToDouble(inchesBox.Text);
+            double centimeters = inches * 2.54;
+
+            outputLabel.Text = $"{inches} Inches = {centimeters} Centimeters!";
+        }
+    }
+}
