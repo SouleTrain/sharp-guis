9343241307a73879b23ac2ef5ba002473328add5
Xavier Soule <xavierjsoule@gmail.com>
2026-02-28 21:29:24 -0500

    Add files via upload

---
+++ Lab 4.1 CheckCredit/CheckCredit/CheckCredit/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CheckCredit
+{
+    partial class CheckCredit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.creditBox = new System.Windows.Forms.TextBox();
+            this.payButton = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 25);
+            this.label1.TabIndex = 999;
+            this.label1.Text = "Enter Purchase Price : $";
+            // 
+            // creditBox
+            // 
+            this.creditBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.creditBox.Location = new System.Drawing.Point(306, 78);
+            this.creditBox.Margin = new System.Windows.Forms.Padding(4, 1, 4, 1);
+            this.creditBox.Name = "creditBox";
+            this.creditBox.Size = new System.Drawing.Size(118, 26);
+            this.creditBox.TabIndex = 0;
+            // 
+            // payButton
+            // 
+            this.payButton.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payButton.Location = new System.Drawing.Point(228, 131);
+            this.payButton.Margin = new System.Windows.Forms.Padding(4, 1, 4, 1);
+            this.payButton.Name = "payButton";
+            this.payButton.Size = new System.Drawing.Size(124, 49);
+            this.payButton.TabIndex = 1;
+            this.payButton.Text = "Pay";
+            this.payButton.UseVisualStyleBackColor = true;
+            this.payButton.Click += new System.EventHandler(this.payButton_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(155, 207);
+            this.label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 31);
+            this.label.TabIndex = 1000;
+            this.label.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // CheckCredit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 269);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.payButton);
+            this.Controls.Add(this.creditBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 1, 4, 1);
+            this.Name = "CheckCredit";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox creditBox;
+        private System.Windows.Forms.Button payButton;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Lab 4.1 CheckCredit/CheckCredit/CheckCredit/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckCredit
+{
+    public partial class CheckCredit : Form
+    {
+        public CheckCredit()
+        {
+            InitializeComponent();
+        }
+
+        private void payButton_Click(object sender, EventArgs e)
+        {
+            int credit = Convert.ToInt32(creditBox.Text);
+            if (credit > 8000)
+            {
+                label.Text = $"Error, Credit Declined";
+            }
+            else
+            {
+                label.Text = $"Approved";
+            }
+        }
+    }
+}
