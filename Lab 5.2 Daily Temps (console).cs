b22d79b065be270fedcdf4e9ef4a6ac47eadf2e0
Xavier Soule <xavierjsoule@gmail.com>
2026-02-28 21:33:31 -0500

    Add files via upload

---
+++ Lab 5.2 Daily Temps (console).cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+class DailyTemps
+{
+    static void Main()
+    {
+        bool close = false;
+        int tempSolution = 0;
+
+        while (!close)
+        {
+            //characters here will break this program
+            Console.Write("\nEnter daily high temps >> ");
+            int intInput = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (intInput > 130 || intInput < -20)
+            {
+                Console.WriteLine("\nLets use real temps here friend");
+            }
+            
+            else
+            {
+                //sentinal value
+                Console.WriteLine("Exit and sum temps? Y / N");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "y" or "Y":
+                        tempSolution += intInput;
+                        Console.WriteLine($"{tempSolution} is the total of all daily temps!");
+                        close = true;
+                        break;
+                    case "n" or "N":
+                        tempSolution += intInput;
+                        continue;
+                }
+
+            }
+        }
+    }
+}
+        
+            
+
+        
+    
---
+++ Lab 5.3 Greenville (console).cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Greenville
+{
+    static void Main()
+    {
+        bool loop = true;
+        while (loop)
+        {
+            // last year
+            Console.Write("Enter last years revenue >> ");
+            int lastYearContestants = Convert.ToInt32(Console.ReadLine());
+            if (lastYearContestants < 0 || lastYearContestants > 30)
+            {
+                Console.WriteLine("invalid input");
+                continue;
+            }          
+            int lastYearRevenue = lastYearContestants * 25;
+            string formatLastYear = lastYearRevenue.ToString("c");
+
+            // this year
+            Console.Write("Enter this years revenue >> ");
+            int thisYearContestants = Convert.ToInt32(Console.ReadLine());
+
+            if ((thisYearContestants < 0) || (thisYearContestants > 30))
+            {
+                Console.WriteLine("invalid input");
+                continue;
+            }
+            int thisYearRevenue = thisYearContestants * 25;
+            string formatThisYear = thisYearRevenue.ToString("c");
+
+            //output
+            Console.WriteLine($"\nThere were {thisYearContestants} and {formatThisYear} this year.\n" +
+                              $"Along with {lastYearContestants} and {formatLastYear} last year.");
+            
+            if (thisYearContestants >  lastYearContestants)
+            {
+                Console.WriteLine("\nThere were more contestants this year!");
+            }
+            else if (thisYearContestants < lastYearContestants)
+            {
+                Console.WriteLine("\nThere were more contestants last year, whoops!");
+            }
+            else
+            {
+                Console.WriteLine("\nThere were the same amount of contestants this year\n" +
+                                   "as last year. I refuse to believe this!");
+            }
+            loop = false;
+        }   
+    }
+}
